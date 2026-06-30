8a2e60e2b77c74200727baeb938c87cdb0b26364
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-30 22:24:47 +0300

    Introduce render target pooling on cameras

--- src/plugins/camera/nodes/cameraview.js
+++ src/plugins/camera/nodes/cameraview.js
@@ -1,7 +1,10 @@
 import { Camera, Object3D } from "../../../objects/index.js"
 import { View, Views } from "../../../renderer/index.js"
+import { CanvasTarget } from "../../../rendertarget"
 import { Vector3 } from "../../../math/index.js"
 import { assert } from "../../../utils/index.js"
+import { RenderTarget2DPool } from "../RenderTarget2DPool.js"
+import { TextureFormat } from "../../../constants"
 
 export class CameraViewNode {
   subgraph() {
@@ -14,8 +17,11 @@ export class CameraViewNode {
   execute(context) {
     const { objects, renderer } = context
     const views = renderer.getResource(Views)
+    const targetPool = renderer.getResource(RenderTarget2DPool)
 
     assert(views, "Views resource missing")
+    assert(targetPool, "Render target pool resource missing")
+
     for (let i = 0; i < objects.length; i++) {
       const camera = /**@type {Object3D} */(objects[i])
 
@@ -29,7 +35,7 @@ export class CameraViewNode {
         camera.transform.world.z
       )
       const cameraView = new View({
-        renderTarget: camera.target,
+        renderTarget: getTarget(camera, targetPool),
         near: camera.near,
         far: camera.far,
         projection: camera.projection.asProjectionMatrix(camera.near, camera.far),
@@ -43,4 +49,21 @@ export class CameraViewNode {
       views.push(cameraView)
     }
   }
+}
+
+/**
+ * @param {Camera} camera
+ * @param {RenderTarget2DPool} pool
+ */
+function getTarget(camera, pool) {
+  if (camera.target instanceof CanvasTarget) {
+    return pool.get({
+      width: camera.target.canvas.width,
+      height: camera.target.canvas.height,
+      color: [TextureFormat.RGBA8Unorm],
+      depth: TextureFormat.Depth24Plus,
+    })
+  }
+
+  return camera.target
 }
