5e6be29e13f40d0c100bdded900f814e19b5e5a9
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-07-01 18:24:12 +0300

    Add `WebGLPipelineLayout`

---
+++ src/core/layouts/pipeline.js
@@ -0,0 +1,46 @@
+/** @import { WebGLBindGroup, WebGLBindGroupLayout } from "../webgl/bindgroup.js" */
+
+export class WebGLPipelineLayout {
+  /**
+   * @readonly
+   * @type {string | undefined}
+   */
+  label
+
+  /**
+   * @readonly
+   * @type {readonly WebGLBindGroupLayout[]}
+   */
+  bindGroupLayouts
+
+  /**
+   * @param {WebGLPipelineLayoutDescriptor} descriptor
+   */
+  constructor({ label, bindGroupLayouts }) {
+    this.label = label
+    this.bindGroupLayouts = [...bindGroupLayouts]
+  }
+
+  /**
+   * @param {number} index
+   */
+  getBindGroupLayout(index) {
+    return this.bindGroupLayouts[index]
+  }
+
+  /**
+   * @param {number} index
+   * @param {WebGLBindGroup} bindGroup
+   */
+  isBindGroupCompatible(index, bindGroup) {
+    const layout = this.getBindGroupLayout(index)
+
+    return layout ? layout.compatibleWith(bindGroup.layout) : false
+  }
+}
+
+/**
+ * @typedef WebGLPipelineLayoutDescriptor
+ * @property {string} [label]
+ * @property {WebGLBindGroupLayout[]} bindGroupLayouts
+ */
